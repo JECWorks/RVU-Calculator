51ba5d64698aabb436cf94e4763c39e6e65e479b
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-07-20 22:14:01 -0400

    Initial Commit

---
+++ RVU Calculator/RVU_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RVU_CalculatorApp.swift
+//  RVU Calculator
+//
+//  Created by Jason Cox on 7/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RVU_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
